d838328508c1ef344af3c0474851996967b93ea7
jorge machado <jrms_jorgesilva@hotmail.com>
2019-08-15 14:21:19 -0300

    some changes on the pages display and added dinamic links to about-us and privacy policy

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes() ?> >
 <head>
-    <meta charset="UTF-8">
+    <meta charset="<?php bloginfo('charset') ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <?php 
@@ -9,7 +9,7 @@
     ?>
     <title>Fictional University</title>
 </head>
-<body>
+<body <?php body_class(); ?>>
     <header class="site-header">
         <div class="container">
         <h1 class="school-logo-text float-left"><a href="<?php echo site_url() ?>"><strong>Fictional</strong> University</a></h1>
--- page.php
+++ page.php
@@ -37,13 +37,34 @@
                 }
             ?>
             
-            <!-- <div class="page-links">
-            <h2 class="page-links__title"><a href="#">About Us</a></h2>
-            <ul class="min-list">
-                <li class="current_page_item"><a href="#">Our History</a></li>
-                <li><a href="#">Our Goals</a></li>
-            </ul>
-            </div> -->
+            <?php
+            $testArray = get_pages(array(
+                'child_of' => get_the_ID()
+            ));
+            
+            if ($parent or $testArray) { ?>
+            <div class="page-links">
+                <h2 class="page-links__title"><a href="<?php echo get_permalink($parent); ?>"><?php the_title(); ?></a></h2>
+                <ul class="min-list">
+                    <?php
+                        if ($parent) {
+                            $findChildrenOf = $parent;
+                        } else {
+                            $findChildrenOf = get_the_ID();
+                        }
+                        
+
+                        wp_list_pages(
+                            array(
+                            'title_li' => NULL,
+                            'child_of' => $findChildrenOf,
+                            'sort_column' => 'menu_order'
+                            )
+                        );
+                    ?>
+                </ul>
+            </div>
+            <?php } ?>
 
             <div class="generic-content">
             <?php the_content(); ?>
