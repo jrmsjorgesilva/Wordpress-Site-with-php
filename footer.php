b9dfb3ba760275375f063e5e32bbaf89bd680d5e
jorge machado <jrms_jorgesilva@hotmail.com>
2019-08-15 16:18:11 -0300

    added post funtionality and adjusted the navbar items

--- footer.php
+++ footer.php
@@ -13,6 +13,11 @@
       <div class="site-footer__col-two">
         <h3 class="headline headline--small">Explore</h3>
         <nav class="nav-list">
+          <!-- COMENTADO PARA PROSSEGUIR COM O CURSO COM HARDCODED MENU -->
+          <!-- <?php wp_nav_menu(array(
+            'theme_location' => 'footerLocationOne'
+          )); ?> -->
+          <!-- COMENTADO PARA PROSSEGUIR COM O CURSO COM HARDCODED MENU -->
           <ul>
             <li><a href="<?php echo site_url('/index.php/about-us') ?>">About Us</a></li>
             <li><a href="#">Programs</a></li>
@@ -25,6 +30,11 @@
       <div class="site-footer__col-three">
         <h3 class="headline headline--small">Learn</h3>
         <nav class="nav-list">
+          <!-- COMENTADO PARA PROSSEGUIR COM O CURSO COM HARDCODED MENU -->
+          <!-- <?php wp_nav_menu(array(
+            'theme_location' => 'footerLocationTwo'
+          )); ?> -->
+          <!-- COMENTADO PARA PROSSEGUIR COM O CURSO COM HARDCODED MENU -->
           <ul>
             <li><a href="#">Legal</a></li>
             <li><a href="<?php echo site_url('/index.php/privacy-policy') ?>">Privacy</a></li>
--- header.php
+++ header.php
@@ -17,13 +17,20 @@
         <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
         <div class="site-header__menu group">
             <nav class="main-navigation">
-            <ul>
-                <li><a href="<?php echo site_url('/index.php/about-us') ?>">About Us</a></li>
-                <li><a href="#">Programs</a></li>
-                <li><a href="#">Events</a></li>
-                <li><a href="#">Campuses</a></li>
-                <li><a href="#">Blog</a></li>
-            </ul>
+                <!-- COMENTADO PARA PROSSEGUIR COM O CURSO COM HARDCODED MENU -->
+                <!-- <?php  
+                    wp_nav_menu(array(
+                        'theme_location' => 'headerMenuLocatio n'
+                    ));
+                ?> -->
+                <!-- COMENTADO PARA PROSSEGUIR COM O CURSO COM HARDCODED MENU -->
+                <ul>
+                    <li <?php if (is_page('about-us') or wp_get_post_parent_id(0) == 11) echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/index.php/about-us') ?>">About Us</a></li>
+                    <li><a href="#">Programs</a></li>
+                    <li><a href="#">Events</a></li>
+                    <li><a href="#">Campuses</a></li>
+                    <li><a href="#">Blog</a></li>
+                </ul>
             </nav>
             <div class="site-header__util">
             <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
